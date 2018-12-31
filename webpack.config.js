f6c3f1dc6a9d1cb4b34fbe65bb4a3f4450680a52
Igor Mandello <igormandello@gmail.com>
2018-12-31 05:49:05 -0200

    Replaces babel with webpack
    - Webpack uses the babel-loader plugin

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    filename: 'index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
