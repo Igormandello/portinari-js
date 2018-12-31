12f1a29ed3fb4ea36250c9eb3d490badf38c29a3
Igor Mandello <igormandello@gmail.com>
2018-12-31 08:59:30 -0200

    Adds an image rendering to example

--- example/main.js
+++ example/main.js
@@ -1,15 +1,27 @@
-let ge = new GameEngine(document.getElementById('cnv'), 16 / 9, 1);
+let ge = new GameEngine(document.getElementById('cnv'), 16 / 9, 60),
+    res = new Resources('assets', [ 'foo.png' ]),
+    input = new Input(() => true);
 
-ge.addUpdateComponent((ctx, ratio) => {
+ge.addUpdateComponent((ctx) => {
   ctx.fillStyle = '#000';
   ctx.fillRect(0, 0, ge.canvas.width, ge.canvas.height);
-}, 'mainUpdate');
+}, 'firstUpdate');
+
+let imagePos = { x: 0, y: 0 }, speed = 5;
+ge.addUpdateComponent((ctx, ratio) => {
+  imagePos.x += input.axis.horizontal * speed;
+  imagePos.y += input.axis.vertical * speed;
+
+  ctx.drawImage(res.images['foo.png'], imagePos.x, imagePos.y);
+}, 'secondUpdate');
 
 ge.addUpdateComponent((ctx, ratio, frameCount) => {
-  if (frameCount % 2) {
+  if (frameCount < 200) {
     ctx.fillStyle = '#009999';
     ctx.fillRect(ge.canvas.width / 4, ge.canvas.height / 4, ge.canvas.width / 2, ge.canvas.height / 2);
   }
-}, 'secondaryUpdate');
+}, 'thirdUpdate');
 
-ge.start();
+res.onload = () => {
+  ge.start();
+}
