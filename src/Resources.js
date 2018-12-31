dac342001fd86d75b84f853d263879045dd44d45
Igor Mandello <igormandello@gmail.com>
2018-12-31 08:59:18 -0200

    Creates the Resources class to load images

---
+++ src/Resources.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+class Resources {
+  constructor(rootDir, images) {
+    let remaining = images.length;
+    let imageLoaded = () => {
+      remaining--;
+      
+      if (remaining == 0 && this.onload && typeof this.onload === 'function')
+        this.onload();
+    }
+
+    this._images = {};
+    images.forEach(filename => {
+      let img = new Image();
+      img.onload = imageLoaded;
+
+      img.src = path.join(rootDir, filename);
+      this._images[filename] = img;
+    });
+  }
+
+  get images() {
+    return this._images;
+  }
+}
+
+module.exports = Resources;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const GameEngine = require('./GameEngine');
 const Input = require('./Input');
 const Keys = require('./Keys');
+const Resources = require('./Resources');
 
 module.exports = {
   GameEngine,
   Input,
-  Keys
+  Keys,
+  Resources
 };
